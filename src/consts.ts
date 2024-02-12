f1df04c2293f66a47b56db6413e6152593b20636
Pol Gubau <gubaupol@gmail.com>
2024-02-12 11:52:51 +0100

    first commit

---
+++ src/consts.ts
@@ -0,0 +1,17 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+// With the move to TSUP as a build tool, this keeps path routes in other files (installers, loaders, etc) in check more easily.
+// Path is in relation to a single index.js file inside ./dist
+const __filename = fileURLToPath(import.meta.url);
+const distPath = path.dirname(__filename);
+export const PKG_ROOT = path.join(distPath, "../");
+
+
+export const TITLE_TEXT = `   ___ ___ ___   __ _____ ___   _____ ____    __   ___ ___
+  / __| _ \\ __| /  \\_   _| __| |_   _|__ /   /  \\ | _ \\ _ \\
+ | (__|   / _| / /\\ \\| | | _|    | |  |_ \\  / /\\ \\|  _/  _/
+  \\___|_|_\\___|_/‾‾\\_\\_| |___|   |_| |___/ /_/‾‾\\_\\_| |_|
+`;
+export const DEFAULT_APP_NAME = "pol-ui";
+export const COMMAND_NAME = "create-pol-ui";
